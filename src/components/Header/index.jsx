606f2fd79c1ec89dd5984b06b0d8f56f3ec7786b
Matthew <smith0223@ymail.com>
2021-01-21 19:58:42 -0500

    added package..completed table and header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function Header() {
+    return (
+        <div>
+            <header>
+                <h1>UserDirectory</h1>
+                <p></p>
+            </header>
+        </div>
+    )
+}
+export default Header;
