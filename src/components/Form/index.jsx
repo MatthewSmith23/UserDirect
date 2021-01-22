d2a699ab2d889c3f9ebe5de94ea6ab2520d9e45f
Matthew <smith0223@ymail.com>
2021-01-21 19:47:46 -0500

    form folder js and css updated

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css';
+
+function Form(props) {
+    return (
+        <div>
+            <nav class="navbar navbar-dark bg-dark">
+                <div class="container-fluid-center">
+                    <form class="d-flex">
+                        <input name="search"
+                            class="form-control me-2"
+                            type="search" placeholder="Search"
+                            value={props.search} onChange={props.handleInputChange} />
+                    </form>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Form;
