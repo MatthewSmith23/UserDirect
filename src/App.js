ba09a0615469455c1e1a2520dcc46c38d7269268
Matthew <smith0223@ymail.com>
2021-01-21 19:30:23 -0500

    create react app..added function to app.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Router basename="/UserDirect">
+      <div>
+        <Directory />
+    </div>
+    </Router>
+  );
+}
+
+export default App;
