2b792e4c6b6b4e5c7508ebcd16cc3e255291ee6c
Matthew <smith0223@ymail.com>
2021-01-21 19:41:00 -0500

    addded function and imports to pages

---
+++ src/pages/Directory.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Table from '../components/Table/index.jsx';
+import Header from '../components/Header/index.jsx';
+
+function Directory() {
+    return (
+        <div>
+            <Header />
+            <Table />
+        </div>
+    )
+}
+
+export default Directory;
